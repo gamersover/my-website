29ba6ba689499db8a5f2502faf93d93e75f688d8
gamersover <cmathking@gmail.com>
2023-09-02 13:24:33 +0800

    first commit

---
+++ components/About.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+        <div className="h-screen flex flex-col justify-center items-center dark:bg-black">
+            <p className="dark:text-white">建设中</p>
+        </div>
+    )
+}
