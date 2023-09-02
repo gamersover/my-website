5b81193b778c4d6434f4048482a69b531ee27bc8
gamersover <cmathking@gmail.com>
2023-09-02 14:35:40 +0800

    移动端UI优化

--- components/Home.tsx
+++ components/Home.tsx
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col justify-center items-center dark:bg-black">
       <p className="text-5xl mb-10 dark:text-white">欢迎来到我的网站</p>
-      <div className="flex gap-8 justify-center">
+      <div className="flex flex-wrap gap-8 justify-center">
         <Card
           isPressable
           onPress={() => {
             window.location.replace("https://blog.caoqinping.com");
           }}
-          className="bg-amber-100/50 dark:bg-amber-800/50 w-1/3 h-full hover:-translate-y-3 transition-transform"
+          className="bg-amber-100/50 dark:bg-amber-800/50 w-[90%] md:w-1/3 h-1/2 md:h-full hover:-translate-y-3 transition-transform"
         >
           <CardHeader className="flex-col !items-center">
             <p className="font-bold text-amber-800 dark:text-amber-100">
@@ -25,7 +25,7 @@ export default function Home() {
           onPress={() => {
             window.location.replace("https://game.caoqinping.com");
           }}
-          className="bg-sky-100/50 dark:bg-sky-800/50 w-1/3 h-full hover:-translate-y-3 transition-transform"
+          className="bg-sky-100/50 dark:bg-sky-800/50 w-[90%] md:w-1/3 h-1/2 md:h-full hover:-translate-y-3 transition-transform"
         >
           <CardHeader className="flex-col !items-center">
             <p className="font-bold text-sky-800 dark:text-sky-100">扑克游戏</p>
